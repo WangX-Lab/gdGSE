f766a3e793369d1c3b162788a40e1a70be1cb4a6
WangX-Lab <54932820+WangX-Lab@users.noreply.github.com>
2025-03-25 20:27:39 +0800

    Add files via upload

---
+++ R/data.r
@@ -0,0 +1,43 @@
+#' Title for Bulk_exp_matrix
+#'
+#' The expression matrix of the samples or single cells for which enrichment scoring needs to be performed.
+#'
+#' @format The expression matrix of 45 samples and 20531 genes of TCGA-CHOL normal and tumor.
+"Bulk_exp_matrix"
+
+#' Title for Bulk_condition
+#'
+#' The conditions of bulk sample state or cell annotation of single-cell transcriptomes.
+#'
+#' @format The conditions of bulk sample state from TCGA-CHOL,including tumor and normal.
+"Bulk_condition"
+
+#' Title for Bulk_Signature
+#'
+#' A list of signatures,for example:the Gene Ontology Biological Process(GOBP) gene sets, cell type-specific signatures or a gene list correlated with interested features.
+#'
+#' @format 100 Gene Ontology Biological Process(GOBP) gene sets.
+"Bulk_Signature"
+
+#' Title for SC_exp_matrix
+#'
+#' The expression matrix of the single cells for which enrichment scoring needs to be performed.
+#'
+#' @format The expression matrix of 1000 cells and 5000 genes from GSE149614.
+"SC_exp_matrix"
+
+#' Title for SC_condition
+#'
+#' The cell annotation of the single cells for from GSE149614.
+#'
+#' @format The expression matrix of 1000 cells and 5000 genes from GSE149614.
+"SC_condition"
+
+#' Title for SC_Signature
+#'
+#' The cell marker gene sets of the single cells for from GSE149614.
+#'
+#' @format The cell marker gene sets of the single cells for from GSE149614.
+"SC_Signature"
+
+
